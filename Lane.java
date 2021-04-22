13cb73d4758b30497246766589c905b3ebe93368
Jack Van <80840796+jackvan20000312@users.noreply.github.com>
2021-04-21 20:06:00 -0400

    Finished vehicle and lane

--- Lane.java
+++ Lane.java
@@ -5,67 +5,81 @@ package edu.mtc.egr283.ProjectExtraCredit01;
  */
 public class Lane {
 
-    private Vehicle[] theLane;
+	private Vehicle[] theLane;
 
-    /**
-     *  Constructs a  Lane-object with place for n vehicles
-     */
-    public Lane(int n){
-    	this.theLane = new Vehicle[n];
-    }
+	/**
+	 *  Constructs a  Lane-object with place for n vehicles
+	 */
+	public Lane(int n){
+		this.theLane = new Vehicle[n];
+	}
 
-    /**
-     * Step all vehicles (except the one in place 0) one step
-     * (if possible). (The vehicle at position 0 is removed before calling
-     * this method using the method below).
-     */
-    public void step(){
-    	for(int i=1; i<theLane.length; i++) {
-    		theLane[i-1]=theLane[i];
-    		theLane[i]=null;
-    	}
-    }
+	/**
+	 * Step all vehicles (except the one in place 0) one step
+	 * (if possible). (The vehicle at position 0 is removed before calling
+	 * this method using the method below).
+	 */
+	public void step(){
+		for(int i=1; i<this.theLane.length; i++) {
+			this.theLane[i-1]=this.theLane[i];
+			this.theLane[i]=null;
+		}
+	}
 
-    /**
-     * Removes the first vehicel
-     * @return The Vehicle at the first place or null if it is empty
-     */
-    public Vehicle removeFirst(){
-    	Vehicle temp= theLane[0];
-    	this.theLane[0]= null;
-        return temp;
-    }
+	/**
+	 * Removes the first vehicle
+	 * @return The Vehicle at the first place or null if it is empty
+	 */
+	public Vehicle removeFirst(){
+		Vehicle temp= theLane[0];
+		this.theLane[0]= null;
+		return temp;
+	}
 
-    /**
-     * Returns the first vehicle without removing it
-     * @return A reference to the vehicle in the first place or null if it is
-     * empty
-     */
-    public Vehicle getFirst(){
-        return this.theLane[0];
-    }
+	/**
+	 * Returns the first vehicle without removing it
+	 * @return A reference to the vehicle in the first place or null if it is
+	 * empty
+	 */
+	public Vehicle getFirst(){
+		return this.theLane[0];
+	}
 
-    /**
-     * @return true if the last place if empty, else false
-     */
-    public boolean lastFree(){
-       
-    	return true;
-    }
+	/**
+	 * @return true if the last place if empty, else false
+	 */
+	public boolean lastFree(){
+		if(this.theLane[this.theLane.length-1]==null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 
-    /**
-     * Stores a vehicle at the end of the lane
-     * @param v The vehicle that is to be stored
-     */
-    public void putLast(Vehicle v)
-    {
-    }
+	/**
+	 * Stores a vehicle at the end of the lane
+	 * @param v The vehicle that is to be stored
+	 */
+	public void putLast(Vehicle v){
+		if(this.lastFree()) {
+			this.theLane[this.theLane.length-1]=v;
+		}
+	}
 
-    /**
-     * @return A string representation of a lane and its vehicles
-     */
-    public String toString()
-    {
-        return null;
-    }
+	/**
+	 * @return A string representation of a lane and its vehicles
+	 */
+	public String toString(){
+		StringBuffer sb = new StringBuffer();
+		sb.append("[");
+		for(int i=0; i<this.theLane.length; i++) {
+			if(this.theLane[i]==null) {
+				sb.append(" ");
+			}else {
+				sb.append(this.theLane[i].toString());
+			}
+		}
+		sb.append("]");
+		return sb.toString();
+	}
 }
--- Vehicle.java
+++ Vehicle.java
@@ -5,22 +5,29 @@ package edu.mtc.egr283.ProjectExtraCredit01;
  */
 public class Vehicle {
 
-    private int bornTime;
-    private char destination;
+	private int bornTime;
+	private Character destination;
 
-    /**
-     *  Constructs a  Vehicle-object with b bornTime and d destination
-     */
-    public Vehicle(int b, char d){
-    	this.bornTime=b;
-    	this.destination=d;
-    }
+	/**
+	 *  Constructs a  Vehicle-object with b bornTime and d destination
+	 */
+	public Vehicle(int b, char d){
+		this.bornTime=b;
+		this.destination=d;
+	}
 
-    /**
-     * @return A String-representation of the vechile and its bornTime and destination
-     */
-    public String toString(){
-    	
-        return (bornTime+", "+destination);
-    }
+	/**
+	 *  Constructs a  Vehicle-object with b bornTime and d destination
+	 */
+	public int getBornTime(){
+		return this.bornTime;
+	}
+
+	/**
+	 * @return A String-representation of the vehicle and its bornTime and destination
+	 */
+	public String toString(){
+
+		return Character.toString(destination);
+	}
 }
