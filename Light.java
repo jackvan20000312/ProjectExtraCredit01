e294baf70a9f24649bd48648c94e67357633cf3a
Jack Van <80840796+jackvan20000312@users.noreply.github.com>
2021-04-21 20:29:25 -0400

    finished light class

--- Light.java
+++ Light.java
@@ -4,37 +4,43 @@ package edu.mtc.egr283.ProjectExtraCredit01;
  *  Represents a traffic signal
  */
 public class Light {
-    private int period;
-    private int green;
-    private int time;
+	private int period;
+	private int green;
+	private int time;
 
-    /**
-     *  Constructs a  Light-object for p period and g green
-     */
-    public Light(int p, int g)
-    {
-    }
+	/**
+	 *  Constructs a  Light-object for p period and g green
+	 */
+	public Light(int p, int g){
+		this.green=g;
+		this.period=p;
+		this.time=0;
+	}
 
-    /**
-     * Steps the clock of the signal
-     */
-    public void step()
-    {
-    }
+	/**
+	 * Steps the clock of the signal
+	 */
+	public void step(){
+		this.time++;
+	}
 
-    /**
-     * @return true if the signal is green otherwise false
-     */
-    public boolean isGreen()
-    {
-        return true;
-    }
+	/**
+	 * @return true if the signal is green otherwise false
+	 */
+	public boolean isGreen(){
+		if(this.time%period<=green) {
+			return true;
+		}
+		return false;
+	}
 
-    /**
-     * @return A String-representation of the signal that shows its color
-     */
-    public String  toString()
-    {
-        return null;
-    }
+	/**
+	 * @return A String-representation of the signal that shows its color
+	 */
+	public String  toString(){
+		if(this.isGreen()) {
+			return"G:";
+		}
+		return "R:";
+	}
 }
